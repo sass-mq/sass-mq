65b00c7be6dba7de24173cc445eec7aaca036ceb
Kaelig <kaelig@deloumeau.fr>
2015-08-03 12:23:09 +0100

    Fix indentation to match the rest of the project

--- test/eyeglass-test.js
+++ test/eyeglass-test.js
@@ -5,14 +5,16 @@ var sass = require('node-sass');
 var Eyeglass = require('eyeglass').Eyeglass;
 
 var sassOptions = {
-  file: './test/test-eyeglass.scss'
+    file: './test/test-eyeglass.scss'
 };
+
 var eyeglass = new Eyeglass(sassOptions);
 
 sass.render(eyeglass.sassOptions(), function(err, res){
-  if (err) console.log(err);
-  fs.writeFile('./test/test-eyeglass.css', res.css.toString(), function(err) {
     if (err) console.log(err);
-    console.log('Wrote CSS to ./test/test-eyeglass.css');
-  });
+
+    fs.writeFile('./test/test-eyeglass.css', res.css.toString(), function(err) {
+        if (err) console.log(err);
+        console.log('Wrote CSS to ./test/test-eyeglass.css');
+    });
 });
