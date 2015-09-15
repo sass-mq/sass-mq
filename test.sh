946ad8f9251275931fe406040ac826221bf02495
Kaelig <kaelig@deloumeau.fr>
2015-09-15 15:58:32 -0700

    Add the ability to override width calculation

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sass test/test.scss test/test.css --load-path 'bower_components/sass-mq' --load-path './'  --sourcemap=none
+sass test/test.scss test/test.css --load-path 'bower_components/sass-mq' --load-path './'  --sourcemap=none --trace
 node-sass test/test.scss test/test.css --include-path 'bower_components/sass-mq' --include-path './' --sourcemap=none
 node test/eyeglass-test.js
