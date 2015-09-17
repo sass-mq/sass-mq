2455a9a99efa1b90a622c59d5d169d56489e8d9d
smbeiragh <smbeiragh@gmail.com>
2015-09-17 17:15:03 +0430

    misspelled correction in width override test in test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sass test/test.scss test/test.css --load-path 'bower_components/sass-mq' --load-path './'  --sourcemap=none --trace
-sass test/test-width-overide.scss test/test-width-overide.css --load-path 'bower_components/sass-mq' --load-path './'  --sourcemap=none --trace
+sass test/test-width-override.scss test/test-width-override.css --load-path 'bower_components/sass-mq' --load-path './'  --sourcemap=none --trace
 node-sass test/test.scss test/test.css --include-path 'bower_components/sass-mq' --include-path './' --sourcemap=none
-node-sass test/test-width-overide.scss test/test-width-overide.css --include-path 'bower_components/sass-mq' --include-path './' --sourcemap=none
+node-sass test/test-width-override.scss test/test-width-override.css --include-path 'bower_components/sass-mq' --include-path './' --sourcemap=none
 node test/eyeglass-test.js
