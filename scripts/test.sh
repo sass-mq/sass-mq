17d03be528ad2f514eeefbd6d8622c2cb7be3d84
Hackzzila <admin@hackzzila.com>
2017-05-17 20:31:04 -0500

    add diamond tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@ sass --update --force test/test.scss:test/output/test-ruby.css --sourcemap=none 
 dart-sass test/test.scss 1>test/output/test-dart.css 2>test/output/dart-sass.log
 node-sass test/test.scss test/output/test-node.css --sourcemap=none --quiet 2>test/output/node-sass.log
 node test/eyeglass-test.js 2>test/output/eyeglass.log
+diamond c -o test/output/test-diamond.css test/test-diamond.scss 2> test/output/diamond.log
 
 DIFF=`git diff --name-only test/output/*.*`
 
