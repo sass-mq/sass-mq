90e0a1fb760be8b8c6347bcea6115950dbb368f1
Kaelig <kaelig.deloumeauprigent@shopify.com>
2018-01-13 10:29:17 -0800

    Generate sassdocs into the /docs folder

--- scripts/sassdoc.sh
+++ scripts/sassdoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Generate documentation and deploy it to GitHub pages
 # https://sass-mq.github.io/sass-mq/
-sassdoc _mq.scss docs --config=.sassdocrc
+sassdoc _mq.scss -d docs --config=.sassdocrc
 git add ./docs
 git commit -m "Compile SassDoc"
