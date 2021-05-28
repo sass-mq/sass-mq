b2bd20d6adcc00a41c055a359ff32f56439ce695
rerades <rerades@siete3.com>
2021-05-29 00:41:28 +0200

    refactoring updating modules

--- test/eyeglass-test.js
+++ test/eyeglass-test.js
@@ -1,23 +1,27 @@
-'use strict';
+"use strict";
 
-var fs = require('fs');
-var sass = require('node-sass');
-var eyeglass = require('eyeglass');
-var path = require('path');
+var fs = require("fs");
+var sass = require("sass");
+var eyeglass = require("eyeglass");
+var path = require("path");
 
 var sassOptions = {
-    file: './test/test-eyeglass.scss'
+    file: "./test/test-eyeglass.scss",
 };
 
 sassOptions.eyeglass = {
-    root: path.join(__dirname, '../'),
-    buildDir: __dirname
+    root: path.join(__dirname, "../"),
+    buildDir: __dirname,
 };
 
-sass.render(eyeglass(sassOptions), function(err, res){
+sass.render(eyeglass(sassOptions), function (err, res) {
     if (err) console.log(err);
 
-    fs.writeFile('./test/output/test-eyeglass.css', res.css.toString(), function(err) {
-        if (err) console.log(err);
-    });
+    fs.writeFile(
+        "./test/output/test-eyeglass.css",
+        res.css.toString(),
+        function (err) {
+            if (err) console.log(err);
+        }
+    );
 });
